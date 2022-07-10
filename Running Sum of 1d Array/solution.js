453550220572765fac320f8f30e708f372758163
bk_bharathikannan <91874993+bharathikannansambasivam@users.noreply.github.com>
2022-07-10 20:02:14 +0530

    1480. Running Sum of 1d Array

---
+++ Running Sum of 1d Array/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+let total=0;
+    let  sum=[];
+
+for(i=0;i<nums.length;i++)
+        {
+       
+           total=nums[i]+total;
+           sum.push(total);
+
+        }
+    return sum;
+};
