76e3922da810efbdc50b7da890edf3f1b7e5cd46
SBharathikannan <bharathikannansambasivam@gmail.com>
2022-07-14 13:47:54 +0530

    1. Two Sum/

---
+++ 1. Two Sum/solution.js
@@ -0,0 +1,20 @@
+let twoSum = function (nums, target) {
+    let index = [];
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j <= nums.length; j++) {
+            let x = nums[i] + nums[j];
+
+            if (x == target) {
+                index = [i, j];
+                return index;
+            }
+        }
+
+    }
+
+};
+
+   //time complexity O(n^2)
+   //space complexity O(n)
+
+
