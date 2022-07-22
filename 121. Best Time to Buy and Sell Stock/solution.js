fb16fca980125ae902661cb2995e0d430cad0c2c
bk_bharathikannan <91874993+bharathikannansambasivam@users.noreply.github.com>
2022-07-22 22:17:40 +0530

    121.Best Time to buy and sell stock

--- 121. Best Time to Buy and Sell Stock/solution.js
+++ 121. Best Time to Buy and Sell Stock/solution.js
@@ -1,15 +1,15 @@
 var maxProfit = function (prices) {
-    let Buy = 0;
+    let buy = 0;
 
     let max_profit = 0;
     for (let sell = 1; sell < prices.length; sell++) {
-        if (prices[Buy] < prices[sell]) {
-            let profit = prices[sell] - prices[Buy];
+        if (prices[buy] < prices[sell]) {
+            let profit = prices[sell] - prices[buy];
 
             max_profit = Math.max(max_profit, profit);
         }
         else {
-            Buy = sell;
+            buy = sell;
         }
 
 
