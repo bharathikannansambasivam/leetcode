d60d9cb416e39f6e96cc05e609deb1a43363f822
bk_bharathikannan <91874993+bharathikannansambasivam@users.noreply.github.com>
2022-07-23 22:26:20 +0530

    136.Single Number

--- 136. Single Number/solution.js
+++ 136. Single Number/solution.js
@@ -2,16 +2,21 @@
  * @param {number[]} nums
  * @return {number}
  */
- var singleNumber = function(nums) {
-    nums=nums.sort()
-    for(i=0;i<nums.length;i++){
-       for(j=i+1;j<nums.length;j++){
-          if(nums[i]==nums[j]){
-            nums.splice(i,2);
-            i--;
-            }
-        }
-        
+var singleNumber = function (nums) {
+  nums = nums.sort();
+  for (i = 0; i < nums.length; i++) {
+    for (j = i + 1; j < nums.length; j++) {
+      if (nums[i] == nums[j]) {
+        nums.splice(i, 2);
+        i--;
+      }
     }
-    return nums;
+  }
+  return nums;
 };
+
+
+
+
+//time compleity O(n^2)
+//space compleity O(1)
