10f0dda31c9a8f3f723252dfcc69b19711b4148a
bk_bharathikannan <91874993+bharathikannansambasivam@users.noreply.github.com>
2022-07-25 14:41:59 +0530

    Final Value of Variable After Performing Operation

--- 2011. Final Value of Variable After Performing Operations/solution.js
+++ 2011. Final Value of Variable After Performing Operations/solution.js
@@ -3,15 +3,16 @@
  * @return {number}
  */
 var finalValueAfterOperations = function (operations) {
-    let ans = 0;
-    for (i = 0; i < operations.length; i++) {
-        if (operations[i] == "X++" || operations[i] == "++X") {
-            ans++;
-        }
-        else if (operations[i] == "--X" || operations[i] == "X--") {
-            ans--;
-        }
+  let ans = 0;
+  for (i = 0; i < operations.length; i++) {
+    if (operations[i] == "X++" || operations[i] == "++X") {
+      ans++;
+    } else if (operations[i] == "--X" || operations[i] == "X--") {
+      ans--;
     }
-    return ans;
-
+  }
+  return ans;
 };
+
+//time complexity O(n)
+//space complexity O(1)
