48de770a23bf326a3ef365a507b6e0fe83992027
bk_bharathikannan <91874993+bharathikannansambasivam@users.noreply.github.com>
2022-07-25 14:24:23 +0530

    Shuffle the Array

--- Shuffle the Array/solution.js
+++ Shuffle the Array/solution.js
@@ -1,12 +1,10 @@
-
- var shuffle = function(nums, n) {
-    let empty=[];
-     for(i=0;i<n;i++){
-       
-     empty.push(nums[i],nums[i+n]);
-     }
-     return empty;
- };
+var shuffle = function (nums, n) {
+  let empty = [];
+  for (i = 0; i < n; i++) {
+    empty.push(nums[i], nums[i + n]);
+  }
+  return empty;
+};
 
 //Time complexity    O(n)
 //Space complexity    O(n)
