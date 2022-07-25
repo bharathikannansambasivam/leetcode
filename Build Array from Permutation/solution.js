355299e316b789db2708a6a6e7a5ab4e23020edb
bk_bharathikannan <91874993+bharathikannansambasivam@users.noreply.github.com>
2022-07-25 14:29:56 +0530

    1920. Build Array from Permutation

--- Build Array from Permutation/solution.js
+++ Build Array from Permutation/solution.js
@@ -1,8 +1,10 @@
-var buildArray = function(nums) {
-    ans=[];
-    for (let i = 0; i < nums.length;i++){
-        ans.push(nums[nums[i]]);
-    }
-    return ans;
-    } 
- 
+var buildArray = function (nums) {
+  ans = [];
+  for (let i = 0; i < nums.length; i++) {
+    ans.push(nums[nums[i]]);
+  }
+  return ans;
+};
+
+//time complexity O(n)
+//space complexity O(n)
