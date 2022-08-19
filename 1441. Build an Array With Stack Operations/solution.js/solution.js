54fb2735b89b476541e8b0aef56422d7835538ac
SBharathikannan <bharathikannansambasivam@gmail.com>
2022-08-19 20:33:36 +0530

    First commit

---
+++ 1441. Build an Array With Stack Operations/solution.js/solution.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} target
+ * @param {number} n
+ * @return {string[]}
+ */
+var buildArray = function (target, n) {
+    let ans = [];
+    let j = 0;
+
+    for (let i = 1; i <= n; i++) {
+        if (j > target.length - 1) {
+            break;
+        }
+        if (target[j] === i) {
+            ans.push("Push");
+            j++;
+        } else ans.push("Push", "Pop");
+    }
+    return ans;
+};
